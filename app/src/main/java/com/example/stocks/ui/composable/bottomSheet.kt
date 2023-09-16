78a5bdae9be2021a16059c24e8e630e6e0364ec7
abhishek <ag7aka@gmail.com>
2023-09-16 22:28:54 +0530

    BottomSheet fixes

--- app/src/main/java/com/example/stocks/ui/composable/bottomSheet.kt
+++ app/src/main/java/com/example/stocks/ui/composable/bottomSheet.kt
@@ -1,10 +1,13 @@
 package com.example.stocks.ui.composable
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -19,9 +22,13 @@ import com.example.stocks.viewmodel.MyViewModel
             modifier = Modifier.background(Color.White),
             scaffoldState = scaffoldState,
             sheetContent = {
-
+                Text(
+                    modifier = Modifier.fillMaxHeight().align(Alignment.CenterHorizontally),
+                    text = "No News Found",
+                    color = Color.White
+                )
             },
-            sheetPeekHeight = 0.dp,
+            sheetPeekHeight = 100.dp
         ) {
             appBar(viewModel = viewModel)
         }
